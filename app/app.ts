9bc7aa89f9085463572746459c6c69f10db604ff
Hamza Moslah <hamzamaslah@gmail.com>
2018-11-12 15:17:23 +0100

    hello hamza

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,6 @@
 import $ from "jquery";
 import Hello from "./hello.ts";
 
-let hello = new Hello("Jamila!!!");
+let hello = new Hello("Hamza!!!");
 
 $("body").append(`greeting: ${hello.greeting()}`);
